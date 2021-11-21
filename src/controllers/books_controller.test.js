103956f3f3aca25497bfab28cb524c479b1f1c7d
Anna W <tester.kodilla@gmail.com>
2021-11-21 13:13:52 +0000

    Deleting a book test

--- src/controllers/books_controller.test.js
+++ src/controllers/books_controller.test.js
@@ -1,4 +1,4 @@
-const {bookService} = require('../services');
+const { bookService } = require('../services');
 jest.mock('../services/books');
 
 
@@ -88,7 +88,7 @@ describe('POST /api/v1/books endpoint', () => {
   test('status code successfully 201 for saving a valid book', async () => {
     // Act
     const res = await request(app).post('/api/v1/books')
-      .send({bookId: 3, title: 'Fantastic Mr. Fox', author: 'Roald Dahl',});
+      .send({ bookId: 3, title: 'Fantastic Mr. Fox', author: 'Roald Dahl', });
 
     // Assert
     expect(res.statusCode).toEqual(201);
@@ -101,9 +101,27 @@ describe('POST /api/v1/books endpoint', () => {
     });
     // Act
     const res = await request(app).post('/api/v1/books')
-      .send({title: 'Fantastic Mr. Fox', author: 'Roald Dahl',}); // No bookId
+      .send({ title: 'Fantastic Mr. Fox', author: 'Roald Dahl', }); // No bookId
 
     // Assert
     expect(res.statusCode).toEqual(400);
+  });
+});
+
+describe('DELETE /api/v1/books/{bookId} endpoint', () => {
+  test('status code successfully 200 for deleting a valid book', async () => {
+    // Act
+    const res = await request(app).delete('/api/v1/books/2');
+
+    // Assert
+    expect(res.statusCode).toEqual(200);
+  });
+
+  test('status code successfully 404 for a book that is not found', async () => {
+    // Act
+    const res = await request(app).delete('/api/v1/books/7');
+
+    // Assert
+    expect(res.statusCode).toEqual(404);
   });
 });
